b8078fa632c66cb173bb01ff860918c1bbb70889
Zenah Chehab <z_ch_83@hotmail.com>
2023-06-09 12:22:33 -0400

    Created changes in ReverseNumber.java

--- src/Zena_Solutions/ReverseNumber.java
+++ src/Zena_Solutions/ReverseNumber.java
@@ -9,7 +9,7 @@ public class ReverseNumber {
 
         while (num != 0) { // -1234 doesnt equal 0
 
-            reverse = reverse*10 + num%10;    // 0+1234%10=4       40+3=43         430+2=432                            4320+1=4321
+            reverse = reverse*10 + num%10; // 0+1234%10=4   40+3=43   430+2=432   4320+1=4321
             num = num / 10;   // 1234/10=123   123/10=12   12/10=1   1/10=0
         }
 
